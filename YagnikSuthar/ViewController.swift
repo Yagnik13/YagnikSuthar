b18c00276f53fbf0204d00f4f5dc054e56151421
Yagnik Suthar <yagniksuthar@ymail.com>
2018-11-21 22:26:08 +0530

    Initial Commit

---
+++ YagnikSuthar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YagnikSuthar
+//
+//  Created by yagnik on 21/11/18.
+//  Copyright © 2018 yagnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
